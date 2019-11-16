5b4615d04917236aa80a86500d81ad634eab15f4
Alexey Evlampev <a.evlampev@innopolis.ru>
2019-11-17 01:37:09 +0200

    save productItem

--- src/pages/productItem.vue
+++ src/pages/productItem.vue
@@ -1,14 +1,39 @@
 <template>
+    <div class="productItem">
 
+    </div>
 </template>
 
 <script>
   export default {
     name: "productItem",
-
+    props: ['data']
   };
 </script>
 
 <style scoped>
+    .productItem {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        &:not(:last-child) {
+            margin-bottom: 10px;
+        }
+        &-img {
+            width: 100px;
+            height: 100px;
+            border-radius: 3px;
+            overflow: hidden;
+        }
+        &-name {
+            margin-left: 10px;
+            flex: 1;
+        }
+        &-counter {
 
+        }
+        &-openTrigger {
+            padding: 10px;
+        }
+    }
 </style>
