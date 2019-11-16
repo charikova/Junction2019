0c46d6616409593c9903d93cb167c47413a4b50d
Alexey Evlampev <a.evlampev@innopolis.ru>
2019-11-16 17:13:15 +0200

    add diet component

---
+++ src/components/headerMain.vue
@@ -0,0 +1,29 @@
+<template>
+    <f7-navbar ref="navbar" class="myheader-navbar">
+        <f7-nav-title class="mytitle">Log In</f7-nav-title>
+        <div class="close">
+            <i class="f7-icons" @click="openedLogin = false">close</i>
+        </div>
+    </f7-navbar>
+</template>
+
+<script>
+  export default {
+    name: "HeaderMain",
+    mounted() {
+      this.$refs.navbar.size();
+    }
+  };
+</script>
+
+<style scoped lang="scss">
+    @import "./../css/main";
+    .myheader-navbar {
+        background: $main-color;
+        height: 72px;
+        color: #fff;
+    }
+    .mytitle {
+        font-size: 20px!important;
+    }
+</style>
---
+++ src/pages/diet.vue
@@ -0,0 +1,19 @@
+<template>
+    <f7-page class="main-page" color-theme="green">
+        <header-main></header-main>
+    </f7-page>
+</template>
+
+<script>
+
+
+  import HeaderMain from "../components/headerMain";
+  export default {
+    name: "diet",
+    components: { HeaderMain },
+  };
+</script>
+
+<style scoped>
+
+</style>
