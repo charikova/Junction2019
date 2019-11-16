32ef0a10e64f87d66b2908f7cb5b9ad0553ab046
Alexey Evlampev <a.evlampev@innopolis.ru>
2019-11-16 21:49:56 +0200

    add i button

--- src/components/progressBar.vue
+++ src/components/progressBar.vue
@@ -1,38 +1,52 @@
 <template>
-    <div class="stats-progress" ref="barRef">
-        <div class="stats-progress-bar" :style="`width: ${percents}%`"></div>
-        <div class="stats-progress-text">
-            Saturated fats: <b>100 g</b>
-        </div>
-    </div>
+  <div class="stats-progress" ref="barRef">
+    <div class="stats-progress-bar" :style="`width: ${percents}%; background: ${color}`"></div>
+    <div class="stats-progress-text">Saturated fats: <b>100 g</b></div>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: "progressBar",
-    props: ['percents']
-  };
+const chartColors = {
+  red: "rgb(255, 99, 132)",
+  orange: "rgb(255, 159, 64)",
+  yellow: "rgb(255, 205, 86)",
+  green: "rgb(75, 192, 192)",
+  blue: "rgb(54, 162, 235)",
+  purple: "rgb(153, 102, 255)",
+  grey: "rgb(201, 203, 207)"
+};
+
+export default {
+  name: "progressBar",
+  props: ["percents"],
+  computed: {
+    color: function() {
+      if (this.percents > 75) return chartColors.red;
+      if (this.percents > 50) return chartColors.yellow;
+      return chartColors.green;
+    }
+  }
+};
 </script>
 
 <style scoped lang="scss">
-    .stats-progress {
-        display: flex;
-        height: 30px;
-        line-height: 30px;
-        color: black;
-        padding: 0 10px;
-        position: relative;
-        &-bar {
-            position: absolute;
-            top: 0;
-            bottom: 0;
-            left: 0;
-            background: rgb(255, 220, 155);
-            border-radius: 5px;
-            z-index: 10;
-        }
-        &-text {
-            z-index: 11;
-        }
-    }
+.stats-progress {
+  height: 30px;
+  line-height: 30px;
+  color: black;
+  padding: 0 10px;
+  position: relative;
+  flex: 1;
+  &-bar {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    background: rgb(255, 220, 155);
+    border-radius: 5px;
+    z-index: -1;
+  }
+  &-text {
+  }
+}
 </style>
--- src/pages/profile.vue
+++ src/pages/profile.vue
@@ -1,11 +1,16 @@
 <template>
   <f7-page style="background: white">
     <div class="stats">
-      <div class="stats-section stats-section-chart">
-        <CircleDiagram />
+      <div class="stats-section ">
+        <div class="stats-section-chart">
+          <CircleDiagram />
+        </div>
       </div>
       <div class="stats-section">
         <progress-bar percents="100" />
+        <div class="stats-i-icon">
+          <i class="f7-icons">info</i>
+        </div>
       </div>
     </div>
   </f7-page>
@@ -22,15 +27,29 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import "../css/main";
 .stats {
   max-width: 500px;
   margin: 0 auto;
   &-section {
     padding: 15px;
     border-bottom: 1px solid #ddd;
+    display: flex;
+    flex-direction: row;
   }
   &-section-chart {
-    padding: 15px 0 15px;
+    width: 80%;
+    box-sizing: border-box;
+    margin: auto;
+  }
+  &-i-icon {
+    color: lighten($main-color, 20);
+    font-size: 18px;
+    margin-left: 10px;
+    cursor: pointer;
+    &:hover {
+      color: $main-color;
+    }
   }
 }
 </style>
