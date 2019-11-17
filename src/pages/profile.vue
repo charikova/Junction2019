8583ca3ae6b0548ea0a090c311522fb563071c2b
Alexey Evlampev <a.evlampev@innopolis.ru>
2019-11-17 03:33:02 +0200

    fix e-numbers

--- src/pages/profile.vue
+++ src/pages/profile.vue
@@ -34,15 +34,15 @@
       </div>
       <div class="stats-section">
         <div class="stats-section-title">Food additives</div>
-        <div class="stats-row" v-for="e in Object.keys(monthData.top)">
+        <div class="stats-row" v-for="e in monthData.top">
           <div>
-            <span class="stats-label" style="background-color: #ff6e7d">{{
-              e
+            <span class="stats-label" :style="`background-color: ${e.color}`">{{
+              e.e
             }}</span>
-            <span style="margin-left: 10px">Tartrazine</span>
+            <span style="margin-left: 10px">{{ e.name }}</span>
           </div>
-          <div class="stats-number" style="border-color: #ff6e7d">
-            {{ monthData.top[e] }}
+          <div class="stats-number" :style="`border-color: ${e.color}`">
+            {{ e.count }}
           </div>
         </div>
       </div>
@@ -77,6 +77,10 @@ export default {
       const result = this.$store.state.statistics.find(
         data => data.month === "10"
       );
+      result.top = result.top.map(item => ({
+        ...item,
+        color: item.danger_level > 3 ? "#ff6e7d" : "#ffc935"
+      }));
       return result;
     }
   }
