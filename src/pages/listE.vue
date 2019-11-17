f5d15f4edd893e041f548e35e29fde08d3688135
Alexey Evlampev <a.evlampev@innopolis.ru>
2019-11-17 07:25:19 +0200

    add block to listE

--- src/pages/listE.vue
+++ src/pages/listE.vue
@@ -1,5 +1,8 @@
 <template>
     <f7-page>
+        <f7-block strong>
+            <h2>asdasd</h2>
+        </f7-block>
         <f7-block-title>Products</f7-block-title>
         <f7-list media-list>
             <f7-list-item
@@ -9,7 +12,7 @@
                     :title="product.name_eng || product.name_fin"
                     :text="product.ing_eng || product.ing_fin"
             >
-                <img slot="media" :src="product.picture_url+'?fit=clip&h=40&w=40'" width="40" />
+                <img slot="media" :src="product.picture_url+'?fit=clip&h=160&w=160'" width="40" />
             </f7-list-item>
         </f7-list>
     </f7-page>
